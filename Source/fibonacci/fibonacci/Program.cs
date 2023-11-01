e65e5abff65147c3b60688aa2850104b9a3467d2
Eric Ung <ungericw@tutanota.com>
2023-11-01 11:23:18 -0500

    stateX stateY stateZ generators

--- Source/fibonacci/fibonacci/Program.cs
+++ Source/fibonacci/fibonacci/Program.cs
@@ -1,4 +1,6 @@
-﻿int fibonacci(int n)
+﻿using System.Runtime.CompilerServices;
+
+int fibonacci(int n)
 {
     if (n == 0)
     {
@@ -19,11 +21,66 @@
     return y;
 }
 
-for (int i = 0; i < 50; i++)
+Console.WriteLine("Generates fibonacci using dynamic programming");
+
+for (int i = 0; i < 25; i++)
 {
     int y = fibonacci(i);
     int eX = i % 2;
     int eY = y % 2;
 
     Console.WriteLine("f(\t" + i + "\t) =\t\t" + y + "\t\tEx = " + eX + "\t\teY = " + eY);
+}
+
+int fibonacciGenerator(int n)
+{
+    int stateX = 0;
+    int stateY = 1;
+    int stateZ = 1;
+    int cycles = 0;
+
+    while (cycles <= n)
+    {
+        cycles++;
+
+        if (cycles > n)
+        {
+            return stateX;
+        }
+
+        stateX = stateY + stateZ;
+        Console.WriteLine("stateX: " + stateX + "\tstateY: " + stateY + "\tstateZ: " + stateY);
+
+        
+        cycles++;
+
+        if (cycles > n)
+        {
+            return stateY;
+        }
+
+        stateY = stateX + stateZ;
+        Console.WriteLine("stateX: " + stateX + "\tstateY: " + stateY + "\tstateZ: " + stateY);
+
+        cycles++;
+
+        if (cycles > n)
+        {
+            return stateZ;
+        }
+
+        stateZ = stateX + stateY;
+        Console.WriteLine("stateX: " + stateX + "\tstateY: " + stateY + "\tstateZ: " + stateY);
+    }
+
+    return stateX;
+}
+
+Console.WriteLine("\n\nGenerates fibonacci sequence using generators");
+
+for (int i = 0; i < 25; i++)
+{
+    int y = fibonacciGenerator(i);
+
+    Console.WriteLine("f(\t" + i + "\t) =\t\t" + y);
 }
